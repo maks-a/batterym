74938a4c01a3cab166b3baf6bf95358241af867c
Ma A <m.arutyunyan@gmail.com>
2017-01-05 21:31:00 +0100

    remove joke

--- main.py
+++ main.py
@@ -5,9 +5,6 @@
 
 import os
 import signal
-import json
-
-from urllib2 import Request, urlopen, URLError
 
 from gi.repository import Gtk as gtk
 from gi.repository import AppIndicator3 as appindicator
@@ -29,25 +26,11 @@ def main():
 
 def build_menu():
   menu = gtk.Menu()
-  item_joke = gtk.MenuItem('Joke')
-  item_joke.connect('activate', joke)
-  menu.append(item_joke)
   item_quit = gtk.MenuItem('Quit')
   item_quit.connect('activate', quit)
   menu.append(item_quit)
   menu.show_all()
   return menu
-
-
-def fetch_joke():
-  request = Request('http://api.icndb.com/jokes/random?limitTo=[nerdy]')
-  response = urlopen(request)
-  joke = json.loads(response.read())['value']['joke']
-  return joke
-
-
-def joke(_):
-  notify.Notification.new("<b>Joke</b>", fetch_joke(), None).show()
 
 
 def quit(_):
