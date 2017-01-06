05c2d7a04e051638718f703ee4885203d46669c9
Ma A <m.arutyunyan@gmail.com>
2017-01-06 20:54:49 +0100

    added background thread

--- main.py
+++ main.py
@@ -4,7 +4,9 @@
 # icons from https://materialdesignicons.com/
 
 import os
+import time
 import signal
+import threading
 
 from gi.repository import Gtk as gtk
 from gi.repository import AppIndicator3 as appindicator
@@ -14,6 +16,22 @@ APPINDICATOR_ID = 'batteryindicator'
 indicator = None
 icon = None
 category = appindicator.IndicatorCategory.SYSTEM_SERVICES
+th_background = None
+th_background_stop = None
+
+
+def run_monitoring(stop_event):
+    while not stop_event.is_set():
+        time.sleep(1)
+
+
+def run_background_monitoring():
+    global th_background
+    global th_background_stop
+    th_background_stop = threading.Event()
+    th_background = threading.Thread(
+        target=run_monitoring, args=[th_background_stop])
+    th_background.start()
 
 
 def set_icon(new_icon):
@@ -28,7 +46,7 @@ def setup_indicator(icon):
         APPINDICATOR_ID, icon, category)
     indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
     indicator.set_menu(build_menu())
-    indicator.set_label('84%', '')
+    indicator.set_label('84% 0:21', '')
     set_icon(icon)
 
 
@@ -42,6 +60,8 @@ def build_menu():
 
 
 def quit(source):
+    if th_background_stop:
+        th_background_stop.set()
     gtk.main_quit()
 
 
@@ -53,6 +73,7 @@ def run():
     signal.signal(signal.SIGINT, signal.SIG_DFL)
     setup_indicator(
         os.path.abspath('ic_battery_charging_white_48dp.png'))
+    run_background_monitoring()
     run_forever()
 
 
