ac7c9883d530970abf4c346c6f7a30ee090cecd4
Ma A <m.arutyunyan@gmail.com>
2017-01-29 19:22:06 +0100

    add periodical battery update

--- biapplet/indicator.py
+++ biapplet/indicator.py
@@ -5,6 +5,8 @@ import log
 import osdata
 import resource
 
+from datetime import datetime, timedelta
+
 from gi.repository import Gtk as gtk
 from gi.repository import GObject as gobject
 from gi.repository import AppIndicator3 as appindicator
@@ -19,6 +21,8 @@ class Battery:
     def __init__(self):
         self._status = None
         self._capacity = None
+        self._update_period = timedelta(minutes=10)
+        self._last_update = datetime.now() - self._update_period
         self.update()
 
     def status(self):
@@ -33,12 +37,17 @@ class Battery:
     def update(self):
         status = osdata.battery_status()
         capacity = osdata.battery_capacity()
-        is_new = (self._status != status
-                  or self._capacity != capacity)
+        is_new_data = (self._status != status
+                       or self._capacity != capacity)
         self._status = status
         self._capacity = capacity
 
-        if is_new:
+        current_time = datetime.now()
+        past_time = current_time - self._last_update
+        is_update_time = past_time >= self._update_period
+
+        if is_new_data or is_update_time:
+            self._last_update = current_time
             self.log()
 
     def log(self):
