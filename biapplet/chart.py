bfa0e6e2297b51f49e4a59686cef37a6ba358bfc
Ma A <m.arutyunyan@gmail.com>
2017-01-29 15:28:12 +0100

    test two colored plot

--- biapplet/chart.py
+++ biapplet/chart.py
@@ -367,16 +367,24 @@ def main():
     xlabels = [0, 2, 4, 6, 8, 10, '12 hours']
     ylabels = ['0 %', '50 %', '100 %']
     chart = Chart(inverseX=True, xlabels=xlabels, ylabels=ylabels)
-    ys = [100, 10, 40]
-    xs = [0, 10, 50]
+
     #import random
     #ys = [random.randrange(0, 100) for i in xrange(200)]
     # c90c28 dark red
     # 2e7eb3 blue
     # fa730c orange
     # 4aa635 green
+
     color = '#2e7eb3'
+    ys = [100, 80, 50]
+    xs = [0, 20, 40]
     chart.add(xs=xs, ys=ys, stroke=color, fill=color)
+
+    color = '#4aa635'
+    ys = [50, 70, 80]
+    xs = [40, 50, 60]
+    chart.add(xs=xs, ys=ys, stroke=color, fill=color)
+
     chart.render_to_svg('test.svg')
 
 
