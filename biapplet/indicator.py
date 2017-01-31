c0ed939b2426c1032b65f693081be66461117ca8
Ma A <m.arutyunyan@gmail.com>
2017-01-31 19:58:25 +0100

    add require_version for Gtk and AppIndicator3

--- biapplet/indicator.py
+++ biapplet/indicator.py
@@ -7,6 +7,10 @@ import resource
 
 from datetime import datetime, timedelta
 
+import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('AppIndicator3', '0.1')
+
 from gi.repository import Gtk as gtk
 from gi.repository import GObject as gobject
 from gi.repository import AppIndicator3 as appindicator
