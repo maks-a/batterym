2814a88c039cf05a660e7a7e2172dae86e4ed485
Ma A <m.arutyunyan@gmail.com>
2017-03-19 13:36:08 +0100

    remove 'biapplet' filename

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ import os
 from distutils.core import setup
 
 
-setup(name="biapplet",
+setup(name="batterym",
       version="0.1.0",
       description="Battery Indicator Application for Ubuntu",
       url='',
       author='',
       author_email='',
       license='',
-      packages=['biapplet'],
+      packages=['batterym'],
       data_files=[
-          ('/usr/share/batterym', ['biapplet.desktop'])],
-      scripts=["bin/biapplet"]
+          ('/usr/share/batterym', ['batterym.desktop'])],
+      scripts=["bin/batterym"]
       )
