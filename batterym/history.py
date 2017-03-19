b524a7b64ce2442b133de1594ec3286c6a4ffec5
Ma A <m.arutyunyan@gmail.com>
2017-03-19 22:06:59 +0100

    add debug smoothing

--- batterym/history.py
+++ batterym/history.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python
 import log
-# import scipy.ndimage
-# import scipy.signal
-# import numpy as np
 
 
 def add_relative_time(data):
@@ -34,23 +31,48 @@ def add_virtual_time(samples, threshold_sec):
     return samples
 
 
-# def smooth_virtual_time(samples):
-#     chunks = separate_by_sequence_id(samples)
-#     k = 5
-#     modes = ['reflect', 'constant', 'nearest', 'mirror', 'wrap']
-#     for chunk in chunks:
-#         ys = [x['capacity'] for x in chunk]
-#         #xs = [x['virtual_time_hour'] for x in chunk]
-#         ys2 = scipy.ndimage.gaussian_filter(ys, k)
-#         #ys2 = scipy.signal.savgol_filter(ys, k, 2)
-#         n = len(ys2)
-#         if n < 2*k:
-#             continue
-#         for i in xrange(0, n):
-#             e = chunk[i]
-#             e['capacity_raw'] = e['capacity']
-#             e['capacity'] = ys2[i]
-#     return [item for sublist in chunks for item in sublist]
+def running_mean(l, N):
+    sum = 0
+    result = [0 for x in l]
+
+    k = len(l)
+    N = min(k, N)
+    for i in xrange(0, N):
+        sum += l[i]
+        result[i] = sum / (i+1)
+
+    for i in range(N, k):
+        sum = sum - l[i-N] + l[i]
+        result[i] = sum / N
+
+    return result
+
+
+def my_filter(a, w):
+    n = len(a)
+    k = int((w-1) / 2)
+    result = [0 for x in a]
+    for i in xrange(0, n):
+        l = max(0, i-k)
+        r = min(n-1, i+k)
+        d = min(i-l, r-i)
+        result[i] = float(a[i-d] + a[i+d]) / 2.0
+
+    return result
+
+
+def smooth_virtual_time(samples):
+    chunks = separate_by_sequence_id(samples)
+    for chunk in chunks:
+        ys = [x['capacity'] for x in chunk]
+        ys = my_filter(ys, 7)
+        # ys = running_mean(ys, 3)
+        n = len(ys)
+        for i in xrange(0, n):
+            e = chunk[i]
+            e['capacity_raw'] = e['capacity']
+            e['capacity'] = ys[i]
+    return [item for sublist in chunks for item in sublist]
 
 
 def separate_by_sequence_id(samples):
@@ -83,11 +105,12 @@ def extract_plot_data(batch):
 
 class History:
 
-    def __init__(self, log_data=[], threshold_sec=15*60):
+    def __init__(self, log_data=[], threshold_sec=15*60, smoothing=False):
         data = add_relative_time(log_data)
         data = sorted(data, key=lambda e: e['relative_time_sec'])
         data = add_virtual_time(data, threshold_sec)
-        #data = smooth_virtual_time(data)
+        if smoothing:
+            data = smooth_virtual_time(data)
         self._data = data
         self._plot_data = []
         self._plot_xoffset = 0
@@ -118,3 +141,57 @@ class History:
 
     def plot_data(self, status):
         return [x for x in self._plot_data if x.get('status') in status]
+
+
+def main():
+    image_path = 'capacity_history_12h.svg'
+
+    history1 = History(log.get_battery())
+    history1.set_plot_data_xlimit(hours=12.0)
+    history1.calculate_plot_data()
+    plot_data1 = {
+        'history charging': history1.plot_data(['Charging', 'Full']),
+        'history discharging': history1.plot_data(['Discharging']),
+    }
+
+    history2 = History(log.get_battery(), smoothing=True)
+    history2.set_plot_data_xlimit(hours=12.0)
+    history2.calculate_plot_data()
+    plot_data2 = {
+        'history charging': history2.plot_data(['Charging', 'Full']),
+        'history discharging': history2.plot_data(['Discharging']),
+    }
+
+    white = '#fff'
+    red = '#f00'
+    blue = '#2e7eb3'
+    light_blue = '#8cc'
+    green = '#4aa635'
+    light_green = '#8c8'
+    ylabels = ['0 %', '25 %', '50 %', '75%', '100 %']
+    xlabels = [0, 2, 4, 6, 8, 10, '12 hours']
+
+    from chart import Chart
+    plot = Chart(xlabels=xlabels, ylabels=ylabels,
+                 inverseX=True, padding_top=30, height=450)
+    plot.set_minimal_canvas([0, 0], [12, 100])
+
+    for p in plot_data1['history charging']:
+        plot.add(xs=p['xs'], ys=p['ys'], stroke=light_green, fill=light_green,
+                 drop=white)
+
+    for p in plot_data1['history discharging']:
+        plot.add(xs=p['xs'], ys=p['ys'], stroke=light_blue, fill=light_blue,
+                 drop=white)
+
+    for p in plot_data2['history charging']:
+        plot.add(xs=p['xs'], ys=p['ys'], stroke=red)
+
+    for p in plot_data2['history discharging']:
+        plot.add(xs=p['xs'], ys=p['ys'], stroke=red)
+
+    plot.render_to_svg(image_path)
+
+
+if __name__ == '__main__':
+    main()
