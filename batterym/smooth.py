0ab74434391099c395f459f45b21189b4d6a0840
Ma A <m.arutyunyan@gmail.com>
2017-03-28 00:07:55 +0200

    smooth: eliminate parallel segments, steep, dilute

--- batterym/smooth.py
+++ batterym/smooth.py
@@ -7,60 +7,78 @@ from scipy.interpolate import interp1d
 import unittest
 
 
-# def find_duplicate_idxs1(a):
-#     result = []
-#     n = len(a)
-#     i = -1
-#     while i < n:
-#         i += 1
-#         for j in xrange(i+1, n):
-#             if a[i] == a[j]:
-#                 continue
-#             if j-i > 1:
-#                 for k in xrange(i+1, j):
-#                     result.append(k)
-#             i = j-1
-#             break
-#     return result
+def duplicate_idxs(x, y):
+    idxs = []
+    n = len(y)
+    i = -1
+    while i < n:
+        i += 1
+        for j in xrange(i+1, n):
+            if y[i] != y[j]:
+                i = j-1
+                break
+            if (j-i) > 1:
+                idxs.append(j-1)
+                continue
+    return idxs
 
 
-# def find_duplicate_idxs(arr):
-#     idxs = []
-#     n = len(arr)
-#     i = -1
-#     while i < n:
-#         i += 1
-#         for j in xrange(i+1, n):
-#             if arr[i] == arr[j] and j+1 < n:
-#                 idxs.append(j)
-#                 continue
-#             i = j-1
-#             break
-#     return idxs
+def dilute(x, y):
+    idxs = []
+    n = len(x)
+    for i in xrange(n):
+        if i == 0 or i+1 == n or i%2!=0:
+            continue
+        idxs.append(i)
+    return idxs
 
 
-# def steps_filter2(x, y):
-#     idxs = find_duplicate_idxs(y)
-#     for i in idxs:
-#         x[i] = y[i] = None
-#     new_x = [j for j in x if j is not None]
-#     new_y = [j for j in y if j is not None]
-#     return new_x, new_y
+def steep(x, y, dx):
+    idxs = []
+    n = len(x)
+    skip = False
+    for i in xrange(2, n-1):
+        j = i-1
+        if skip:
+            skip = False
+            continue
+        if abs(x[j] - x[i]) <= dx:
+            k = j if x[j] < x[i] else i
+            idxs.append(i)
+            skip = True
+    return idxs
+
+
+def eliminate(x, y, index_extractor):
+    idxs = index_extractor(x, y)
+    for i in idxs:
+        x[i] = None
+        y[i] = None
+    new_x = [j for j in x if j is not None]
+    new_y = [j for j in y if j is not None]
+    return new_x, new_y
+
 
 
 def steps_filter(x, y):
+    x, y = eliminate(x, y, duplicate_idxs)
+    # dx = 10.0 / 60.0
+    # x, y = eliminate(x, y, lambda x, y: steep(x, y, dx))
+    x, y = eliminate(x, y, dilute)
     return x, y
 
 
 def run_test(filename):
     x, y = np.loadtxt(filename, skiprows=0).T
+    x = list(x)
+    y = list(y)
     x = [-e for e in x]
     fig, ax = plt.subplots()
 
-    ax.plot(x, y, '+:', color='#aaaaaa')
+    ax.plot(x, y, 'x:', color='red')
 
     x2, y2 = steps_filter(x, y)
-    ax.plot(x2, y2, '-o', color='red')
+    ax.plot(x2, y2, '-o', color='blue')
 
     # modes = ['reflect', 'constant', 'nearest', 'mirror', 'wrap']
     # ax.plot(x, scipy.ndimage.gaussian_filter(y, 5, mode=modes[2]), color='red')
@@ -70,18 +88,23 @@ def run_test(filename):
 
 def main():
     folder = './test/chart/'
-    # run_test(folder + 'data1.txt')
-    # run_test(folder + 'data2.txt')
-    # run_test(folder + 'data3.txt')
-    # run_test(folder + 'data4.txt')
+    run_test(folder + 'data1.txt')
+    run_test(folder + 'data2.txt')
+    run_test(folder + 'data3.txt')
+    run_test(folder + 'data4.txt')
     run_test(folder + 'data5.txt')
 
 
 class MyTest(unittest.TestCase):
 
     def test1(self):
-        self.assertEqual(find_duplicate_idxs([]), [])
-        pass
+        self.assertEqual(duplicate_idxs(None, []), [])
+        self.assertEqual(duplicate_idxs(None, [1]), [])
+        self.assertEqual(duplicate_idxs(None, [1, 1]), [])
+        self.assertEqual(duplicate_idxs(None, [1, 1, 1]), [1])
+        self.assertEqual(duplicate_idxs(None, [1, 1, 1, 2]), [1])
+        self.assertEqual(duplicate_idxs(None, [1, 1, 1, 2, 2]), [1])
+        self.assertEqual(duplicate_idxs(None, [1, 1, 1, 2, 2, 2]), [1, 4])
 
 
 if __name__ == '__main__':
