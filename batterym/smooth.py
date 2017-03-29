5343a1d4fed2e4deee023222bca120011883cb69
Ma A <m.arutyunyan@gmail.com>
2017-03-29 22:00:31 +0200

    smooth: combine approaches in steps_filter

--- batterym/smooth.py
+++ batterym/smooth.py
@@ -7,64 +7,64 @@ import matplotlib.pyplot as plt
 import unittest
 
 
-def duplicate_idxs(x, y):
-    idxs = []
-    n = len(y)
-    i = -1
-    while i < n:
-        i += 1
-        for j in xrange(i+1, n):
-            if y[i] != y[j]:
-                i = j-1
-                break
-            if (j-i) > 1:
-                idxs.append(j-1)
-                continue
-    return idxs
+# def duplicate_idxs(x, y):
+#     idxs = []
+#     n = len(y)
+#     i = -1
+#     while i < n:
+#         i += 1
+#         for j in xrange(i+1, n):
+#             if y[i] != y[j]:
+#                 i = j-1
+#                 break
+#             if (j-i) > 1:
+#                 idxs.append(j-1)
+#                 continue
+#     return idxs
 
 
-def dilute(x, y):
-    idxs = []
-    n = len(x)
-    for i in xrange(n):
-        if i == 0 or i+1 == n or i % 2 != 0:
-            continue
-        idxs.append(i)
-    return idxs
+# def dilute(x, y):
+#     idxs = []
+#     n = len(x)
+#     for i in xrange(n):
+#         if i == 0 or i+1 == n or i % 2 != 0:
+#             continue
+#         idxs.append(i)
+#     return idxs
 
 
-def steep(x, y, dx):
-    idxs = []
-    n = len(x)
-    skip = False
-    for i in xrange(2, n-1):
-        j = i-1
-        if skip:
-            skip = False
-            continue
-        if abs(x[j] - x[i]) <= dx:
-            k = j if x[j] < x[i] else i
-            idxs.append(i)
-            skip = True
-    return idxs
+# def steep(x, y, dx):
+#     idxs = []
+#     n = len(x)
+#     skip = False
+#     for i in xrange(2, n-1):
+#         j = i-1
+#         if skip:
+#             skip = False
+#             continue
+#         if abs(x[j] - x[i]) <= dx:
+#             k = j if x[j] < x[i] else i
+#             idxs.append(i)
+#             skip = True
+#     return idxs
 
 
-def eliminate(x, y, index_extractor):
-    idxs = index_extractor(x, y)
-    for i in idxs:
-        x[i] = None
-        y[i] = None
-    new_x = [j for j in x if j is not None]
-    new_y = [j for j in y if j is not None]
-    return new_x, new_y
+# def eliminate(x, y, index_extractor):
+#     idxs = index_extractor(x, y)
+#     for i in idxs:
+#         x[i] = None
+#         y[i] = None
+#     new_x = [j for j in x if j is not None]
+#     new_y = [j for j in y if j is not None]
+#     return new_x, new_y
 
 
-def steps_filter(x, y):
-    x, y = eliminate(x, y, duplicate_idxs)
-    # dx = 10.0 / 60.0
-    # x, y = eliminate(x, y, lambda x, y: steep(x, y, dx))
-    # x, y = eliminate(x, y, dilute)
-    return x, y
+# def steps_filter1(x, y):
+#     x, y = eliminate(x, y, duplicate_idxs)
+#     # dx = 10.0 / 60.0
+#     # x, y = eliminate(x, y, lambda x, y: steep(x, y, dx))
+#     # x, y = eliminate(x, y, dilute)
+#     return x, y
 
 
 def running_mean(l, N):
@@ -88,7 +88,7 @@ def is_within(a, lo, hi):
     return lo <= a and a <= hi
 
 
-def interpolate(x, y, new_x):
+def interpolate_linear(x, y, new_x):
     n = len(x)
     if n < 2:
         return []
@@ -114,12 +114,64 @@ def interpolate(x, y, new_x):
     return new_y
 
 
-def linspace(lo, hi, sz):
+def linspace(lo, hi, step):
+    sz = int((hi - lo) / step)
     step = 1.0 * (hi-lo) / sz
     return [lo+i*step for i in xrange(0, sz+1)]
 
 
-def run_test(filename):
+def tangent_filter(a, w):
+    n = len(a)
+    k = int((w-1) / 2)
+    result = [0 for x in a]
+    for i in xrange(0, n):
+        l = max(0, i-k)
+        r = min(n-1, i+k)
+        d = min(i-l, r-i)
+        result[i] = float(a[i-d] + a[i+d]) / 2.0
+    return result
+
+
+def sub(a, b):
+    n = len(a)
+    return [a[i]-b[i] for i in xrange(0, n)]
+
+
+def scale(a, k):
+    return [x*k for x in a]
+
+
+def normalize(a):
+    mx = max(a)
+    mn = min(a)
+    d = mx-mn
+    return [(2.0*(x-mn)-d)/d for x in a]
+
+
+def scale_dif(y1, y2, k):
+    d = sub(y1, y2)
+    d = scale(d, k)
+    return sub(y1, d)
+
+
+def steps_filter(x, y):
+    xmin = min(x)
+    xmax = max(x)
+
+    dx = 1.0 / 60.0
+    x2 = linspace(xmin, xmax, dx)
+
+    y2 = interpolate_linear(x, y, x2)
+    y3 = tangent_filter(y2, 10)
+    y4 = scale_dif(y2, y3, 0.5)
+
+    dx = 5.0 / 60.0
+    x5 = linspace(xmin, xmax, dx)
+    y5 = interpolate_linear(x2, y4, x5)
+    return x5, y5
+
+
+def run_test2(filename):
     x, y = np.loadtxt(filename, skiprows=0).T
 
     x = list([-e for e in x])
@@ -127,27 +179,55 @@ def run_test(filename):
     x.reverse()
     y.reverse()
 
-    dx = 10.0 / 60.0
     xmin = min(x)
     xmax = max(x)
+    dx = 1.0 / 60.0
     samps = int((xmax - xmin) / dx)
     #x2 = np.linspace(xmin, xmax, samps)
     #f = interp1d(x, y, kind='linear')
     #y2 = f(x2)
     x2 = linspace(xmin, xmax, samps)
-    y2 = interpolate(x, y, x2)
+    y2 = interpolate_linear(x, y, x2)
     #y2 = running_mean(y2, 3)
+
+    y3 = tangent_filter(y2, 10)
+    y4 = scale_dif(y2, y3, 0.5)
+
+    dx = 5.0 / 60.0
+    samps = int((xmax - xmin) / dx)
+    x5 = linspace(xmin, xmax, samps)
+    y5 = interpolate_linear(x2, y4, x5)
 
     fig, ax = plt.subplots()
 
     ax.plot(x, y, 'x:', color='red')
 
     # x2, y2 = steps_filter(x, y)
-    ax.plot(x2, y2, '-o', color='blue')
+    # ax.plot(x2, y2, '-x', color='green')
+
+    # ax.plot(x2, y3, '-x', color='grey')
+    #ax.plot(x2, y4, '-+', color='grey')
+    ax.plot(x5, y5, '-o', color='blue')
 
     # modes = ['reflect', 'constant', 'nearest', 'mirror', 'wrap']
     # ax.plot(x, scipy.ndimage.gaussian_filter(y, 5, mode=modes[2]), color='red')
 
+    mng = plt.get_current_fig_manager()
+    mng.resize(*mng.window.maxsize())
+    plt.show()
+
+
+def run_test(filename):
+    x, y = np.loadtxt(filename, skiprows=0).T
+    x = list([-e for e in x])
+    y = list(y)
+    x.reverse()
+    y.reverse()
+    x5, y5 = steps_filter(x, y)
+
+    fig, ax = plt.subplots()
+    ax.plot(x, y, 'x:', color='red')
+    ax.plot(x5, y5, '-o', color='blue')
     mng = plt.get_current_fig_manager()
     mng.resize(*mng.window.maxsize())
     plt.show()
@@ -164,28 +244,29 @@ def main():
 
 class MyTest(unittest.TestCase):
 
-    def test_duplicate_idxs(self):
-        self.assertEqual(duplicate_idxs(None, []), [])
-        self.assertEqual(duplicate_idxs(None, [1]), [])
-        self.assertEqual(duplicate_idxs(None, [1, 1]), [])
-        self.assertEqual(duplicate_idxs(None, [1, 1, 1]), [1])
-        self.assertEqual(duplicate_idxs(None, [1, 1, 1, 2]), [1])
-        self.assertEqual(duplicate_idxs(None, [1, 1, 1, 2, 2]), [1])
-        self.assertEqual(duplicate_idxs(None, [1, 1, 1, 2, 2, 2]), [1, 4])
+    # def test_duplicate_idxs(self):
+    #     self.assertEqual(duplicate_idxs(None, []), [])
+    #     self.assertEqual(duplicate_idxs(None, [1]), [])
+    #     self.assertEqual(duplicate_idxs(None, [1, 1]), [])
+    #     self.assertEqual(duplicate_idxs(None, [1, 1, 1]), [1])
+    #     self.assertEqual(duplicate_idxs(None, [1, 1, 1, 2]), [1])
+    #     self.assertEqual(duplicate_idxs(None, [1, 1, 1, 2, 2]), [1])
+    #     self.assertEqual(duplicate_idxs(None, [1, 1, 1, 2, 2, 2]), [1, 4])
 
     def test_interpolate(self):
-        self.assertEqual(interpolate([2, 4], [2, 5], []), [])
-        self.assertEqual(interpolate([2, 4], [2, 5], [3]), [3.5])
-        self.assertEqual(interpolate([2, 4], [2, 5], [2, 3, 4]), [2, 3.5, 5])
-        self.assertEqual(interpolate([2, 2], [5, 5], []), [])
-        self.assertEqual(interpolate([2, 2], [5, 5], [2]), [5])
-        self.assertEqual(interpolate([2, 2], [2, 5], [2]), [2])
-        self.assertEqual(interpolate([1], [2], []), [])
-        self.assertEqual(interpolate([1], [2], [3]), [])
-        self.assertEqual(interpolate([1], [2], [3, 4]), [])
-        self.assertEqual(interpolate([-10, -8], [10, 20], [-9]), [15])
+        self.assertEqual(interpolate_linear([2, 4], [2, 5], []), [])
+        self.assertEqual(interpolate_linear([2, 4], [2, 5], [3]), [3.5])
+        self.assertEqual(interpolate_linear(
+            [2, 4], [2, 5], [2, 3, 4]), [2, 3.5, 5])
+        self.assertEqual(interpolate_linear([2, 2], [5, 5], []), [])
+        self.assertEqual(interpolate_linear([2, 2], [5, 5], [2]), [5])
+        self.assertEqual(interpolate_linear([2, 2], [2, 5], [2]), [2])
+        self.assertEqual(interpolate_linear([1], [2], []), [])
+        self.assertEqual(interpolate_linear([1], [2], [3]), [])
+        self.assertEqual(interpolate_linear([1], [2], [3, 4]), [])
+        self.assertEqual(interpolate_linear([-10, -8], [10, 20], [-9]), [15])
 
 
 if __name__ == '__main__':
-    # main()
+    main()
     unittest.main()
