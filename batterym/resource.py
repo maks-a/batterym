6c049226c7b82c5b4bc47e2d9e0844bd16dec704
Ma A <m.arutyunyan@gmail.com>
2017-04-02 21:59:08 +0200

    improve resource coverage 100%

--- batterym/resource.py
+++ batterym/resource.py
@@ -36,3 +36,49 @@ def icon_filename(capacity, is_charging):
 
 def icon_path(capacity, is_charging):
     return image_path(icon_filename(capacity, is_charging), ui.THEME)
+
+
+#####################################################################
+class MyTest(unittest.TestCase):
+
+    def test_image_path(self):
+        self.assertEqual(image_path('a.png', 'dark'),
+                         '/usr/share/icons/ubuntu-mono-dark/status/22/a.png')
+        self.assertEqual(image_path('a.png', 'light'),
+                         '/usr/share/icons/ubuntu-mono-light/status/22/a.png')
+
+    def test_icon_filename(self):
+        self.assertEqual(icon_filename(100, False), 'battery-100.svg')
+        self.assertEqual(icon_filename(90, False), 'battery-100.svg')
+        self.assertEqual(icon_filename(80, False), 'battery-080.svg')
+        self.assertEqual(icon_filename(70, False), 'battery-080.svg')
+        self.assertEqual(icon_filename(60, False), 'battery-060.svg')
+        self.assertEqual(icon_filename(50, False), 'battery-060.svg')
+        self.assertEqual(icon_filename(40, False), 'battery-040.svg')
+        self.assertEqual(icon_filename(30, False), 'battery-040.svg')
+        self.assertEqual(icon_filename(20, False), 'battery-020.svg')
+        self.assertEqual(icon_filename(10, False), 'battery-020.svg')
+        self.assertEqual(icon_filename(0, False), 'battery-000.svg')
+
+        self.assertEqual(icon_filename(100, True), 'battery-100-charging.svg')
+        self.assertEqual(icon_filename(90, True), 'battery-100-charging.svg')
+        self.assertEqual(icon_filename(80, True), 'battery-080-charging.svg')
+        self.assertEqual(icon_filename(70, True), 'battery-080-charging.svg')
+        self.assertEqual(icon_filename(60, True), 'battery-060-charging.svg')
+        self.assertEqual(icon_filename(50, True), 'battery-060-charging.svg')
+        self.assertEqual(icon_filename(40, True), 'battery-040-charging.svg')
+        self.assertEqual(icon_filename(30, True), 'battery-040-charging.svg')
+        self.assertEqual(icon_filename(20, True), 'battery-020-charging.svg')
+        self.assertEqual(icon_filename(10, True), 'battery-020-charging.svg')
+        self.assertEqual(icon_filename(0, True), 'battery-000-charging.svg')
+
+    def test_icon_path(self):
+        ui.reset_theme()
+        self.assertEqual(icon_path(
+            100, True),
+            '/usr/share/icons/ubuntu-mono-dark/status/22/battery-100-charging.svg')
+
+        ui.toggle_theme()
+        self.assertEqual(icon_path(
+            100, True),
+            '/usr/share/icons/ubuntu-mono-light/status/22/battery-100-charging.svg')
