61b273189e5d3e3ef00d0565f1ddcb9418b9b23a
Ma A <m.arutyunyan@gmail.com>
2017-04-02 22:08:29 +0200

    improve log coverage

--- batterym/log.py
+++ batterym/log.py
@@ -57,3 +57,41 @@ def parse_log_lines(lines):
 def get_battery():
     lines = get_lines(LOG_BATTERY_FILE)
     return filter(lambda line: line is not None, parse_log_lines(lines))
+
+
+#####################################################################
+class MyTest(unittest.TestCase):
+
+    def test_parse_log_line(self):
+        self.assertEqual(parse_log_lines(
+            ['2017-01-09T22:59:17.275801 100% Full']),
+            [{
+                'time': datetime.datetime(2017, 1, 9, 22, 59, 17),
+                'capacity': 100,
+                'status': 'Full',
+            }]
+        )
+        self.assertEqual(parse_log_lines(
+            ['2017-01-09T22:59:17.275801 95% Discharging']),
+            [{
+                'time': datetime.datetime(2017, 1, 9, 22, 59, 17),
+                'capacity': 95,
+                'status': 'Discharging',
+            }]
+        )
+        self.assertEqual(parse_log_lines(
+            ['2017-01-09T22:59:17.275801 46% Charging']),
+            [{
+                'time': datetime.datetime(2017, 1, 9, 22, 59, 17),
+                'capacity': 46,
+                'status': 'Charging',
+            }]
+        )
+        self.assertEqual(parse_log_lines(
+            ['2017-01-09T22:59:17.275801 97% Unknown']),
+            [{
+                'time': datetime.datetime(2017, 1, 9, 22, 59, 17),
+                'capacity': 97,
+                'status': 'Unknown',
+            }]
+        )
