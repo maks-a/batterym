452126582f71c51c60fd9975c69132a6056027b1
Ma A <m.arutyunyan@gmail.com>
2017-04-15 23:16:54 +0200

    add old/new data, charging/discharging, now line

--- explore/data_analysis.py
+++ explore/data_analysis.py
@@ -29,20 +29,53 @@ data = data.sort_values(by='timestamp', ascending=True)
 print data.head()
 
 # Extract capacity time series.
-X_BEGIN = 0
-X_END = 21
+# Parameters
+X_NOW = 9
+X_BEGIN = X_NOW - 5.0
+X_END = X_NOW + 5.0
+Y_MAX = 101
+Y_MIN = 0
+blue = '#2e7eb3'
+light_blue = '#81b1d1'
+green = '#4aa635'
+light_green = '#7db471'
+
 
 y1 = data['capacity'].values
 y2 = data['capacity_raw'].values
+f = data['status'].values
 x = data['virtual_time_hour']
-cap = pd.DataFrame({'capacity': y1, 'capacity_raw': y2}, index=x)
+cap = pd.DataFrame({'capacity': y1, 'capacity_raw': y2, 'status': f}, index=x)
 cap = cap[X_END:X_BEGIN]
+cap_old = cap[X_END:X_NOW]
+cap_new = cap[X_NOW:X_BEGIN]
+
+charging = cap[cap['status'] == 'Charging']
+discharging = cap[cap['status'] == 'Discharging']
+
+charging_old = charging[X_END:X_NOW]
+charging_new = charging[X_NOW:X_BEGIN]
+discharging_old = discharging[X_END:X_NOW]
+discharging_new = discharging[X_NOW:X_BEGIN]
 
 fig, ax = plt.subplots(1, sharex=True)
-ax.plot(cap['capacity_raw'], color='r', marker='x')
-ax.plot(cap['capacity'], color='b', marker='+')
+
+cap_raw_old = cap_old['capacity_raw']
+cap_raw_new = cap_new['capacity_raw']
+ax.fill_between(cap_raw_old.index, 0, cap_raw_old.values, facecolor=blue)
+ax.fill_between(cap_raw_new.index, 0, cap_raw_new.values, facecolor=light_blue)
+
+ax.plot(charging_old['capacity'], color='r', marker='o', linestyle='None')
+ax.plot(discharging_old['capacity'], color='b', marker='o', linestyle='None')
+
+discharging_raw_old = discharging_old['capacity_raw']
+discharging_raw_new = discharging_new['capacity_raw']
+
+
+ax.plot(cap['capacity'], color='b')
+ax.plot([X_NOW, X_NOW], [Y_MIN, Y_MAX], color='r')
 ax.set_xlim(X_BEGIN, X_END)
-ax.set_ylim(0, 105)
+ax.set_ylim(Y_MIN, Y_MAX)
 ax.invert_xaxis()
 
 # Full screen plot window.
