578adc289263cf53a9e0bb741b0ffee51fd77afa
Ma A <m.arutyunyan@gmail.com>
2017-04-15 16:21:43 +0200

    import battery modules, create init DataFrame

--- explore/data_analysis.py
+++ explore/data_analysis.py
@@ -5,3 +5,13 @@ import matplotlib
 
 from matplotlib import pyplot as plt
 
+# Import modules from /batterym/ folder.
+import sys
+sys.path.append('../batterym')
+import log
+
+
+logs = log.get_battery('../logs/capacity_example')
+df = pd.DataFrame(logs)
+
+print df
