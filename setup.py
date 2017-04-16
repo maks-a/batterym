6680e4cf16e8bb567fe325c64498119740a0f2a9
Ma A <m.arutyunyan@gmail.com>
2017-04-16 23:10:02 +0200

    refactoring

--- setup.py
+++ setup.py
@@ -1,29 +1,22 @@
 #!/usr/bin/python
 import os
 from distutils.core import setup
-from batterym.config import get_entry
-from batterym.misc import append_to_file
-from batterym.misc import create_missing_dirs
 
-from batterym.paths import SHARE_APP_DIR
-from batterym.paths import RESOURCES_DIR
-from batterym.paths import CONFIG_DIR
-from batterym.paths import LOGS_DIR
-from batterym.paths import IMAGE_DIR
-from batterym.paths import LOG_BATTERY_FILE
-from batterym.paths import LOG_BATTERY_ALL_FILE
+import batterym.misc as misc
+import batterym.paths as paths
+import batterym.config as config
 
 
 def find_resources(folder):
-    target_folder = os.path.join(RESOURCES_DIR, folder)
+    target_folder = os.path.join(paths.RESOURCES_DIR, folder)
     fnames = os.listdir(folder)
     resources = [os.path.join(folder, x) for x in fnames]
     return (target_folder, resources)
 
 
-version = get_entry('version', 'config/config.json')
+version = config.get_entry('version', 'config/config.json')
 print 'batterym v{0}'.format(version)
-print 'installation path:', RESOURCES_DIR
+print 'installation path:', paths.RESOURCES_DIR
 
 setup(name='batterym',
       version=version,
@@ -33,17 +26,17 @@ setup(name='batterym',
       license='Apache License 2.0',
       packages=['batterym'],
       data_files=[
-          (SHARE_APP_DIR, ['batterym.desktop']),
-          (RESOURCES_DIR, []),
+          (paths.SHARE_APP_DIR, ['batterym.desktop']),
+          (paths.RESOURCES_DIR, []),
           (find_resources('config')),
-          (LOGS_DIR, []),
+          (paths.LOGS_DIR, []),
           (find_resources('img')),
       ],
       scripts=['bin/batterym']
       )
 
-create_missing_dirs(LOGS_DIR)
-append_to_file('', LOG_BATTERY_FILE)
-append_to_file('', LOG_BATTERY_ALL_FILE)
+misc.create_missing_dirs(paths.LOGS_DIR)
+misc.append_to_file('', paths.LOG_BATTERY_FILE)
+misc.append_to_file('', paths.LOG_BATTERY_ALL_FILE)
 
 print 'batterym is successfully installed.'
