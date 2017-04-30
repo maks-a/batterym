4f2689e55272a23fbece8d51b4f198e91320f4dd
Ma A <m.arutyunyan@gmail.com>
2017-04-30 14:53:49 +0200

    refactoring

--- explore/smoothing.py
+++ explore/smoothing.py
@@ -17,39 +17,41 @@ import history
 
 plt.style.use('ggplot')
 
-# logs = log.get_battery('../logs/capacity_example')
-# h = history.History(logs, smoothing=True)
-# hdata = h.data()
+logs = log.get_battery('../logs/capacity_example')
+h = history.History(logs, smoothing=True)
+hdata = h.data()
 
-# data = pd.DataFrame(hdata)
-# data = data.rename(columns={'time': 'timestamp'})
-# data = data.sort_values(by='timestamp', ascending=True)
+data = pd.DataFrame(hdata)
+data = data.rename(columns={'time': 'timestamp'})
+data = data.sort_values(by='timestamp', ascending=True)
 
-# # Extract charging sessions
-# data = data[data['status'] == 'Charging']
+# Extract charging sessions
+data = data[data['status'] == 'Charging']
 
-# # Group by sequence_id
-# # and leave the longest sessions 
-# grouped = data.groupby('sequence_id')['capacity_raw'].size()
-# grouped = grouped.sort_values(inplace=False, ascending=False)
-# grouped = grouped[:3]
-# data = data[data.sequence_id.isin(grouped.index)]
+# Group by sequence_id
+# and leave the longest sessions
+grouped = data.groupby('sequence_id')['capacity_raw'].size()
+grouped = grouped.sort_values(inplace=False, ascending=False)
+grouped = grouped[:3]
+data = data[data.sequence_id.isin(grouped.index)]
 
-#print data
+print data
 
-x = np.linspace(0, 10, 500)
-y = 10 * np.sin(x)
-y2 = np.round(y)
 
-fig, ax = plt.subplots()
+# def draw_round():
+#     x = np.linspace(0, 10, 500)
+#     y = 10 * np.sin(x)
+#     y2 = np.round(y)
 
-ax.set_ylim(-11, 11)
-ax.plot(x, y, color='b', marker='+')
-ax.plot(x, y2, color='r', marker='o')
+#     fig, ax = plt.subplots()
 
-# Full screen plot window.
-mng = plt.get_current_fig_manager()
-mng.resize(*mng.window.maxsize())
+#     ax.set_ylim(-11, 11)
+#     ax.plot(x, y, color='b', marker='+')
+#     ax.plot(x, y2, color='r', marker='o')
 
-# Show plot.
-plt.show()
+#     # Full screen plot window.
+#     mng = plt.get_current_fig_manager()
+#     mng.resize(*mng.window.maxsize())
+
+#     # Show plot.
+#     plt.show()
